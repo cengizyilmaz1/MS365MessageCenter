eae8e7f3b33b4d52f94e41a8b9512e8a20032288
Cengiz YILMAZ <62932614+cengizyilmaz1@users.noreply.github.com>
2025-05-29 12:02:26 +0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/microsoft-message-center-viewer/' : '/',
+  base: mode === 'production' ? '/MS365MessageCenter/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
